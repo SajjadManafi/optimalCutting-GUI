a3e4cfa4bf2ea1f49dda2e607e155486473b914c
SajjdManafi <sajadmanafi8@gmail.com>
2021-08-02 19:24:00 +0430

    Circle v1.0 (calculate and get Area)

---
+++ src/sample/Circular.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.scene.shape.Circle;
+public class Circular extends Circle implements Shape{
+
+    private double area;
+    private double actualArea;
+
+    public Circular(double centerX, double centerY, double radius) {
+        super(centerX, centerY, radius);
+    }
+
+    @Override
+    public void calculateArea() {
+        actualArea = Math.PI * (Math.pow(this.getRadius() , 2));
+        area = Math.pow(this.getRadius() * 2 , 2);
+
+    }
+    @Override
+    public double getArea() {
+        return area;
+    }
+
+    @Override
+    public double getActualArea() {
+        return actualArea;
+    }
+}
