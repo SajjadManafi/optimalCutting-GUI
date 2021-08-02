7a51b138328b03eeabefe14b2c53c399532a8b8c
SajjdManafi <sajadmanafi8@gmail.com>
2021-08-02 19:13:03 +0430

    Material

---
+++ src/sample/Material.java
@@ -0,0 +1,40 @@
+package sample;
+
+public class Material {
+    private final double width;
+    private final double height;
+
+    private final double InitialArea;
+    private double area;
+    private double actualArea;
+
+
+    public Material(double width, double height) {
+        this.width = width;
+        this.height = height;
+        this.InitialArea = width * height;
+        this.area = this.InitialArea;
+        this.actualArea = this.InitialArea;
+    }
+
+    public void cut(Shape shape){
+        setArea(getArea() - shape.getArea());
+        setActualArea(getActualArea() - shape.getActualArea());
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public double getActualArea() {
+        return actualArea;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public void setActualArea(double actualArea) {
+        this.actualArea = actualArea;
+    }
+}
