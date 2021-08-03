eb9561e497fd945a9ac08339c0bfbc099ba922b6
SajjdManafi <sajadmanafi8@gmail.com>
2021-08-03 18:27:10 +0430

    test bin algorithm

--- src/sample/Show.java
+++ src/sample/Show.java
@@ -27,6 +27,38 @@ public class Show extends Application {
     @FXML
     public void initialize(){
         layout.setPrefSize(Config.config.getWidth() , Config.config.getHeight());
+        Shape rect = new Rectangular(100,100);
+        Node bb = new sample.Node();
+        Shape rect2 = new Rectangular(250,100);
+        Shape rect3 = new Rectangular(450,200);
+        Shape rect4 = new Circular(0,0,240);
+
+        Node.addShape(rect2);
+        Node.addShape(rect);
+        Node.addShape(rect3);
+        Node.addShape(rect4);
+
+        bb.sort();
+        bb.fit();
+//        layout.getChildren().add((javafx.scene.Node) Node.shapes.get(0));
+//        layout.getChildren().add((javafx.scene.Node) Node.shapes.get(1));
+        System.out.println(Node.shapes.get(0).getFit().getX() + " " + Node.shapes.get(0).getFit().getY());
+        System.out.println(Node.shapes.get(1).getFit().getX() + "-" + Node.shapes.get(1).getFit().getY());
+        System.out.println(Node.shapes.get(2).getFit().getX() + "---" + Node.shapes.get(2).getFit().getY());
+        System.out.println(Node.shapes.get(3).getFit().getX() + "---" + Node.shapes.get(3).getFit().getY());
+        System.out.println(Node.shapes.get(0).getShapeWidth());
+        Node.shapes.get(0).setShapeX(Node.shapes.get(0).getFit().getX());
+        Node.shapes.get(0).setShapeY(Node.shapes.get(0).getFit().getY());
+        Node.shapes.get(1).setShapeX(Node.shapes.get(1).getFit().getX());
+        Node.shapes.get(1).setShapeY(Node.shapes.get(1).getFit().getY());
+        Node.shapes.get(2).setShapeX(Node.shapes.get(2).getFit().getX());
+        Node.shapes.get(2).setShapeY(Node.shapes.get(2).getFit().getY());
+        Node.shapes.get(3).setShapeX(Node.shapes.get(3).getFit().getX());
+        Node.shapes.get(3).setShapeY(Node.shapes.get(3).getFit().getY());
+        layout.getChildren().add((javafx.scene.Node) Node.shapes.get(0));
+        layout.getChildren().add((javafx.scene.Node) Node.shapes.get(1));
+        layout.getChildren().add((javafx.scene.Node) Node.shapes.get(2));
+        layout.getChildren().add((javafx.scene.Node) Node.shapes.get(3));
     }
 
 }
